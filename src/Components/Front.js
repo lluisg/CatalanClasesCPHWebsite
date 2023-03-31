68d448cafea3cf2ed1fdb731d6aeee18037ccbc8
lluisg <lluis.guardia.f@gmail.com>
2023-03-31 14:35:31 +0200

    front and about responsive

--- src/Components/Front.js
+++ src/Components/Front.js
@@ -3,30 +3,22 @@ import logo from '../assets/logo.png';
 const Front = (props) => {
   return (
     <div id="front-section" className='section front-section'>
-      <div className="bg-overlay"></div>
-      <div className="front-body">
-        <div className='container-fluid'>
-          <div className='row align-items-center'>
-            <div className='col-1'></div>
-            <div className='col-6'>
-              <h2 className='text-front'>
-                <a style={{fontFamily:'Comfortaa'}}>LEARN CATALAN</a>
-                <br/>
-                <a style={{fontFamily:'Caveat'}}>in Copenhagen</a>
-              </h2>
-            </div>
-          </div>
-          <div className="row justify-content-center" style={{marginTop: '100px'}}>
-            <div className="col-5">
-              <div className="bubble">
-                <div className="btn2contact">
-                  <a>Classes start on the 17-04-23</a>
-                  <a>Limited number of students</a>
-                  <a>Contact Us!</a>
-                  <a href='#contact-section'><span className="link2contact"></span></a>
-                </div>
-              </div>
-            </div>
+      <div className='container-front'>
+        <div className='div-title'>
+          <h2 className='text-front'>
+            {/* <a style={{fontFamily:'Comfortaa'}}>LEARN CATALAN</a> */}
+            <p className='title'>LEARN CATALAN</p>
+            {/* <br/> */}
+            <p className='subtitle'>in Copenhagen</p>
+            {/* <a style={{fontFamily:'Caveat'}}>in Copenhagen</a> */}
+          </h2>
+        </div>
+        <div className="bubble">
+          <div className="btn2contact">
+            <a>Classes start on the 17-04-23</a>
+            <a>Limited number of students</a>
+            <a>Contact Us!</a>
+            <a href='#contact-section'><span className="link2contact"></span></a>
           </div>
         </div>
       </div>
